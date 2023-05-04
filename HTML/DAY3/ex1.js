c075af615707b05e21142d7c1305a7ddfa08555d
GaneshBalki <128831667+GaneshBalki@users.noreply.github.com>
2023-05-04 16:35:11 +0530

    Add files via upload

---
+++ HTML/DAY3/ex1.js
@@ -0,0 +1,17 @@
+let str1 =new String("Ganesh") //premitie type
+let str2=new String("Ganesh") //object type
+if(str1.valueOf(str1)===str2.valueOf(str2))
+{
+    console.log("both are same")
+
+}
+else
+console.log("Not Same")
+
+
+let s1=new String("1")
+let x=1
+if(s1===x)
+  console.log("same")
+  else
+  console.log("not same")
