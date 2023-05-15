dfafcefeeb83cae8d71fb40ddadad78f0f215819
GaneshBalki <ganeshbalki2017@gmail.com>
2023-05-15 23:42:33 +0530

    2

---
+++ React/Greeting.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Welcome from './Welcome';
+function Greeting(){
+    return(<div>
+        Namaskar Bhau............!!
+        <p>You are Noe Viewing the containt of Welcome<Welcome/></p>
+    </div>)
+}
+export default Greeting;
